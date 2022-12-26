eddb4d6d7eedcdf080bc229416fcb8f972b5338c
Arif Widayana <arifwidayana@gmail.com>
2022-12-26 13:55:47 +0700

    constant object data

---
+++ app/src/main/java/com/arifwidayana/notebookstore/common/utils/Constant.kt
@@ -0,0 +1,10 @@
+package com.arifwidayana.notebookstore.common.utils
+
+object Constant {
+    // Database
+    const val STORE_DB = "store.db"
+
+    //Datastore Preference
+    const val DATASTORE_PREF = "DATASTORE_PREF"
+    const val NAME_PREF = "NAME_PREF"
+}
